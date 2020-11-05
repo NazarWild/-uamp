2d5c1952a50440a8e88839989bd743abb56f7790
Nazar <53997458+NazarWild@users.noreply.github.com>
2020-11-05 15:03:36 +0200

    my first time

---
+++ app/src/mainwindow.cpp
@@ -0,0 +1,120 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent), ui(new Ui::MainWindow),
+    m_model(new QFileSystemModel(this))
+{
+    ui->setupUi(this);
+    //ui->menuBar->setNativeMenuBar(false);
+    //setWindowTitle("VENTANA DE MUERTE");
+    
+    ui->treeView->setModel(m_model);
+    ui->treeView->setHeaderHidden(true);
+    ui->treeView->setSelectionBehavior (QAbstractItemView::SelectRows);
+    ui->treeView->setContextMenuPolicy(Qt::CustomContextMenu);
+    for (int i = 1; i < m_model->columnCount(); i++)
+        ui->treeView->hideColumn(i);
+    //QObject::connect(ui->treeView, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(elementClicked(QModelIndex)));
+}
+
+// void MainWindow::elementClicked(const QModelIndex& current) {
+//     QModelIndex p = current;
+//     std::vector<QString> vec;
+//     QString tmp_path;
+
+//     while(p.isValid()) {
+//         vec.push_back(p.data().toString());
+//         p = p.parent();
+//     }
+//     std::reverse(vec.begin(), vec.end());
+//     for (const auto& i : vec) {
+//         if (i.toStdString() != "/")
+//             tmp_path += "/" + i;
+//     }
+//     m_path_file = tmp_path;
+//     TagLib::FileRef f(tmp_path.toStdString().c_str());
+
+//     if(f.file()->isValid()) {
+//         ui->audioFileName->setText(current.data().toString());
+//         ui->textEdit->setText(TStringToQString(f.tag()->artist()));
+//         ui->textEdit_1->setText(TStringToQString(f.tag()->title()));
+//         ui->textEdit_2->setText(TStringToQString(f.tag()->album()));
+//         ui->textEdit_3->setText(TStringToQString(f.tag()->genre()));
+//     }
+// }
+
+MainWindow::~MainWindow()
+{
+    delete m_model;
+    delete ui;
+}
+
+void MainWindow::on_actionOpen_Folder_triggered()
+{
+    QFileDialog dialog(this);
+    dialog.setDirectory(QDir::homePath()); 
+    dialog.setFileMode(QFileDialog::Directory);
+    QString dirname = dialog.getExistingDirectory();
+
+    if (dirname.isEmpty())
+        return ;
+
+    m_path_dir = dirname;
+    m_path_file.clear();
+
+    m_model->setRootPath(dirname); 
+    ui->treeView->setRootIndex(m_model->index(dirname));
+}
+
+void MainWindow::on_actionOpen_File_triggered() {
+    QFileDialog dialog(this);
+    dialog.setDirectory(QDir::homePath()); 
+    dialog.setFileMode(QFileDialog::Directory);
+    QString filename = dialog.getOpenFileName();
+
+    QFile file(filename);
+
+    if (file.open(QFile::ReadOnly | QFile::Text)) {
+
+        m_path_file = filename;
+        QFileInfo fileInfo(filename);
+        m_path_dir = fileInfo.dir().absolutePath();
+
+        m_model->setRootPath(m_path_dir); 
+        ui->treeView->setRootIndex(m_model->index(m_path_dir));
+    }
+}
+
+// void MainWindow::on_actionSave_triggered()
+// {
+//     TagLib::FileRef f(m_path_file.toStdString().c_str());
+
+//     if(f.file()->isValid()) {
+//         QString buffer = ui->textEdit->toPlainText();
+//         f.tag()->setArtist(QStringToTString(buffer));
+//         buffer = ui->textEdit_1->toPlainText();
+//         f.tag()->setTitle(QStringToTString(buffer));
+//         buffer = ui->textEdit_2->toPlainText();
+//         f.tag()->setAlbum(QStringToTString(buffer));
+//         buffer = ui->textEdit_3->toPlainText();
+//         f.tag()->setGenre(QStringToTString(buffer));
+//         f.save();
+//     }
+// }
+
+// void MainWindow::on_actionSave_as_triggered()
+// {
+//     QFileDialog dialog(0);
+//     dialog.setWindowModality(Qt::WindowModal);
+//     dialog.setAcceptMode(QFileDialog::AcceptSave);
+//     if (dialog.exec() == QDialog::Accepted) {
+//         QFile file(dialog.selectedFiles().first());
+//         QFileInfo fileInfo(dialog.selectedFiles().first());
+
+//         m_path_dir = fileInfo.dir().absolutePath();
+//         m_model->setRootPath(m_path_dir);
+//         ui->treeView->setRootIndex(m_model->index(m_path_dir));
+//     }
+// }
+
---
+++ app/src/mainwindow.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QMenuBar>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QDir>
+#include <QFileSystemModel>
+#include <QApplication>
+#include <QMessageBox>
+#include <QWidget>
+#include <QTreeWidget>
+#include <QPushButton>
+#include <QStringList>
+#include <QModelIndex>
+#include <QResizeEvent>
+#include <QDebug>
+#include <QFileInfo>
+#include <QPlainTextEdit>
+#include <QPalette>
+#include <QLabel>
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+#include "main_lib.h"
+
+#include <tag.h>
+#include <tagutils.h>
+#include <tagunion.h>
+#include <fileref.h>
+#include <taglib_export.h>
+#include <audioproperties.h>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots: // all actions 
+    void on_actionOpen_Folder_triggered(); 
+
+    void on_actionOpen_File_triggered();
+
+    // void on_actionSave_triggered();
+
+    // void on_actionSave_as_triggered();
+
+    // void elementClicked(const QModelIndex& current);
+
+private:
+    Ui::MainWindow *ui;
+    QString m_path_file;
+    QString m_path_dir;
+    QFileSystemModel *m_model;
+};
